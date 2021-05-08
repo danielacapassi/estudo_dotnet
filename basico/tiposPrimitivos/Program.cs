8e5c37a3b0a20623a2e947ccd40b44a66e83ab1c
danielacapassi <danielacapssi_nutricao@hotmail.com>
2021-05-08 14:24:08 -0300

    fist commit

---
+++ basico/tiposPrimitivos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace tiposPrimitivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Inteiros
+            int signedInteger;// -2.147.483.648 a 2.147.483.647
+            uint unsignedInteger; // 0 a 4.294.967.295
+            byte unsignedByte;  //	0 a 255
+            sbyte signedByte; //-128 a 127
+            short signedShort; //-32,768 to 32,767
+
+            //Atribuições
+            signedInteger = -2147483647;
+            unsignedInteger = 4999222;
+
+            //Pontos flutuantes
+
+            //Booleano
+
+            bool boolean; 
+
+        }
+    }
+}
---
+++ exerciciosLógica/exercicio18/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace exercicio18
+{
+    class Program
+    {
+        static void Main(string[] args)
+ {
+
+		/*
+		 * Ler o ano atual e o ano de nascimento de uma pessoa. Escrever uma mensagem
+		 * que diga se ela poderá ou não votar este ano (não é necessário considerar o
+		 * mês em que a pessoa nasceu).
+		 */
+
+		int anoAtual;
+		int anoNascimento;
+		int idadeEmAnos;
+
+
+		Console.WriteLine("Qual é o ano atual ? ");
+		anoAtual = Convert.ToInt32(Console.ReadLine());
+
+		Console.WriteLine("Qual é o ano do seu nascimento ? ");
+		anoNascimento = Convert.ToInt32(Console.ReadLine());
+
+		idadeEmAnos = anoAtual - anoNascimento;
+
+		if (idadeEmAnos >= 18) {
+			Console.WriteLine("Você pode votar este ano!");
+		} else {
+			Console.WriteLine("Você tem " + idadeEmAnos + " anos , não pode votar este ano!");
+		}
+
+	}
+    }
+}
