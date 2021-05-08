80a1ca312651a91bfdd28cb01dcab5d9149b34e2
danielacapassi <danielacapssi_nutricao@hotmail.com>
2021-05-08 18:40:09 -0300

    first commit

--- basico/casting/Program.cs
+++ basico/casting/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿//Quando você atribui um valor de um tipo de dados a outro tipo.
+
+//Explicit Casting (manualmente) - conversão de um tipo maior para um tipo de tamanho menor
+//double-> float-> long-> int->char
+
+using System;
 
 namespace casting
 {
@@ -6,7 +11,14 @@ namespace casting
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+      int myInt = 80;
+      double myDouble = 5.50;
+      bool myBool = false;
+
+      Console.WriteLine(Convert.ToString(myInt));    // Convert int to string
+      Console.WriteLine(Convert.ToDouble(myInt));    // Convert int to double
+      Console.WriteLine(Convert.ToInt32(myDouble));  // Convert double to int
+      Console.WriteLine(Convert.ToString(myBool));   // Convert bool to string
         }
     }
 }
