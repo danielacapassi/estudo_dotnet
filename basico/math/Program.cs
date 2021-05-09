3d7df01df484fda02102f01422f1ddf6ed659e21
danielacapassi <danielacapssi_nutricao@hotmail.com>
2021-05-08 23:00:17 -0300

    first commit

---
+++ basico/math/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace math
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //Math.Max :
+        //O método pode ser usado para encontrar o maior valor de x e y :Math.Max(x,y)
+        Console.WriteLine(Math.Max(5, 10));  
+        //Math.Min :
+        //O método pode ser usado para encontrar o menor valor de x e y :Math.Min(x,y)
+        Console.WriteLine(Math.Min(5, 10));
+        //Math.Sqrt :
+        //O método retorna a raiz quadrada de x :Math.Sqrt(x)
+        Console.WriteLine(Math.Sqrt(64));  
+        //Math.Abs :
+        //O método retorna o valor absoluto (positivo) de x :Math.Abs(x)
+        Console.WriteLine(Math.Abs(-4.7));
+        //Math.Round :
+        //Math.Round() arredonda um número para o número inteiro mais próximo:
+        Console.WriteLine(Math.Round(9.99));
+        }
+    }
+}
