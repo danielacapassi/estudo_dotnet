100250cd289489fd816916074640097686a2e375
danielacapassi <danielacapssi_nutricao@hotmail.com>
2021-05-10 08:38:35 -0300

    first commit

---
+++ basico/booleans/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace booleans
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        // bool 
+          int x = 10;
+          int y = 9;
+          Console.WriteLine(x < y);
+
+          Console.WriteLine(10 > 9);
+
+          int w = 10;
+          Console.WriteLine(w == 10);
+
+        }
+    }
+}
---
+++ basico/ifElse/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ifElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+      //
+      int x = 20;
+      int y = 18;
+      if (x > y)
+      {
+        Console.WriteLine("x é maior que  y");
+      }
+      //
+       if (20 > 18) 
+      {
+        Console.WriteLine("20 é maior que 18");
+      }  
+      
+      //
+      int time = 20;
+      if (time < 18) 
+      {
+        Console.WriteLine("Bom dia.");
+      } 
+      else 
+      {
+        Console.WriteLine("Boa noite.");
+      }
+      
+      //
+      int periodoDia = 22;
+      if (periodoDia < 10) 
+      {
+        Console.WriteLine("Bom dia.");
+      } 
+      else if (periodoDia < 20) 
+      {
+        Console.WriteLine("Boa tarde.");
+      } 
+      else 
+      {
+        Console.WriteLine("Boa noite.");
+      }
+      //operador ternário (if...else abreviado): variable = (condition) ? expressionTrue :  expressionFalse;
+      int periodoDiaResumido = 20;
+      string result = (periodoDiaResumido < 18) ? "Bom dia." : "Boa tarde.";
+      Console.WriteLine(result);
+        }
+    }
+}
