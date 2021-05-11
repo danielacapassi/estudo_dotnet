1f8eb603cae2126a26152b2f7c0451dcb31b4c60
danielacapassi <danielacapssi_nutricao@hotmail.com>
2021-05-11 08:30:17 -0300

    first commit

---
+++ basico/whileloop/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace whileloop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //while loop
+        int i = 0;
+        while (i < 5) 
+        {
+        Console.WriteLine(i);
+        i++;
+        }
+
+
+        // Do While
+        int e = 0;
+       do 
+      {
+        Console.WriteLine(e);
+        e++;
+      }
+      while (i < 5);
+    }
+    }
+}
+
+
