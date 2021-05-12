156aa5708ef6be04d5075ae8c6cd621f09f3f1d7
danielacapassi <danielacapssi_nutricao@hotmail.com>
2021-05-11 22:52:28 -0300

    first commit

---
+++ basico/breakUso/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace breakUso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++) 
+        {
+        if (i == 4) 
+        {
+            break;
+        }
+        Console.WriteLine(i);
+        }
+
+        Console.WriteLine("-------------------");
+
+       // CONTINUE
+        for (int e = 0; e < 10; e++) 
+        {
+        if ( e == 4) 
+        {
+            continue;
+        }
+        Console.WriteLine(e);
+        }
+        Console.WriteLine("-------------------");
+
+        //Break and Continue in While Loop
+        int a = 0;
+        while (a < 10) 
+        {
+        Console.WriteLine(a);
+        a++;
+        if (a == 4) 
+        {
+            break;
+        }
+        }
+        Console.WriteLine("-------------------");
+       
+        int o = 0;
+        while (o < 10) 
+        {
+        if (o == 4) 
+        {
+            o++;
+            continue;
+        }
+        Console.WriteLine(o);
+        o++;
+        }
+                        }
+                    }
+                }
---
+++ basico/forloop/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace forloop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // numeros pares
+         for (int i = 0; i <= 10; i = i + 2)
+      {
+        Console.WriteLine(i);
+        }
+      
+       Console.WriteLine ("-------------------------");
+
+       // numeros até 4
+        for (int e = 0; e < 5; e++) 
+{
+  Console.WriteLine(e);
+}
+
+ Console.WriteLine ("-------------------------");
+
+// foreach
+string[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+foreach (string a in cars) 
+{
+  Console.WriteLine(a);
+}
+
+    }
+}
+}
